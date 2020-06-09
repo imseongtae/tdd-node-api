99d1a427ab737e2999b9ea625f258e9a9009c987
imseongtae <dev.haemil@gmail.com>
2020-06-09 16:12:28 +0900

    style: Lint code

--- 1-basic-of-node/index.js
+++ 1-basic-of-node/index.js
@@ -4,23 +4,23 @@ const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req.url)
+	console.log(req.url);
 
-  if (req.url === '/') {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World! \n');
-  } else if (req.url === '/users') { // get users라는 API 추가
-    res.statusCode = 201;
-    res.setHeader('Content-Type', 'text/plain'); 
-    res.end('User list \n') // end함수로 User list 문자열 응답
-  } else {
-    res.statusCode = 404;
-    res.end('404 Not Found~! \n')
-  }
-  
+	if (req.url === '/') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('Hello, World! \n');
+	} else if (req.url === '/users') {
+		// get users라는 API 추가
+		res.statusCode = 201;
+		res.setHeader('Content-Type', 'text/plain');
+		res.end('User list \n'); // end함수로 User list 문자열 응답
+	} else {
+		res.statusCode = 404;
+		res.end('404 Not Found~! \n');
+	}
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+	console.log(`Server running at http://${hostname}:${port}/`);
 });
